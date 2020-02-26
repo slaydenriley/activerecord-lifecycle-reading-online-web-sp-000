2ec8ec1c1d72573fdd08291b04db575e01d5c24f
Riley Slayden <slaydenriley@gmail.com>
2020-02-26 22:50:54 +0000

    Automatically backed up by Learn

--- app/models/post.rb
+++ app/models/post.rb
@@ -17,4 +17,9 @@ class Post < ActiveRecord::Base
   def make_title_case
     self.title = self.title.titlecase
   end
+
+  def email_author_about_post
+    # Not implemented.
+    # For more information: https://guides.rubyonrails.org/action_mailer_basics.html
+  end
 end
